94d2bb8a209b304a9992bc89a65227eb8b32cbef
jacobchase <jchase996@gmail.com>
2025-04-22 11:57:24 -0500

    created a header component

---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+    <header className="header">
+    <img src = "src/assets/claude.png" className="logo"/>
+    <h1 className="title">Chef Claude</h1>
+    </header>
+    )
+}
