a86bf81057f23076eff3b89be06882af41467295
jacobchase <jchase996@gmail.com>
2025-04-22 16:00:07 -0500

    added a placeholder list of ingredients

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,10 +1,23 @@
 export default function body(){
+    var ingredients = ["Mayonnaise", "Lettuce"]
+    function addIngredient(event){
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        ingredients.push(newIngredient)
+    }
+    const ingredientsList = ingredients.map((ingredient) => 
+        <li>{ingredient}</li>
+    )
     return(
         <main>
-            <form className="IngredientForm">
-                <input type="text" placeholder="e.g. oregano" aria-label="add-ingredient"/>
+            <form onSubmit={addIngredient} className="IngredientForm">
+                <input type="text" placeholder="e.g. oregano" aria-label="add-ingredient" name="Ingredient"/>
                 <button>+ Add Ingredient</button>
             </form>
+            <ul>
+            {ingredientsList}
+            </ul>
         </main>
     )
 }
